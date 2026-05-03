eb1620080d039b1c4d841678b4fdb3df4bd4e3ff
sreekanth <mdev01@kvbl.in>
2026-05-03 20:40:03 +0530

    version mistake

--- lib/core/static.dart
+++ lib/core/static.dart
@@ -1,5 +1,5 @@
 class Static {
-  // static const String baseUrl = 'https://api.delybelllogistics.com/';
-  static const String baseUrl = 'https://dev.api.delybelllogistics.com/';
+  static const String baseUrl = 'https://api.delybelllogistics.com/';
+  // static const String baseUrl = 'https://dev.api.delybelllogistics.com/';
   // static const String baseUrl = 'https://new.api.delybell.com/';
 }
